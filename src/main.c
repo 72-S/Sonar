e7b2cd1cb61ebae1826d9c2f6a4826133aa8c1d1
72-S <consti.bauer@outlook.com>
2024-12-26 15:15:39 +0100

    simple window

--- src/main.c
+++ src/main.c
@@ -1,6 +1,24 @@
-#include <stdio.h>
+#include "gio/gio.h"
+#include "glib-object.h"
+#include "glib.h"
+#include <gtk/gtk.h>
 
-int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+
+static void activate(GtkApplication *app, gpointer data) {
+    GtkWidget *window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Sonar");
+    gtk_widget_set_visible(window, true);
+}
+
+int main(int argc, char *argv[]) {
+    GtkApplication *app;
+    int status;
+
+    app = gtk_application_new("dev.consti.sonar", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+
+    return status;
 }
